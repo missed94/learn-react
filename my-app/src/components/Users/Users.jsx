4151a9bd0cffa460d94ea3c7b48234fe6ffb3b0e
Ivan Filatov <J_23_23_J@211.ru>
2020-10-08 15:13:40 +0700

    53 урок

--- my-app/src/components/Users/Users.jsx
+++ my-app/src/components/Users/Users.jsx
@@ -9,6 +9,7 @@ const Users = (props) => {
     let getUsers = () => {
         if (props.users.length === 0) {
             axios.get("https://social-network.samuraijs.com/api/1.0/users")
+
                 .then(response => {
                     props.onSetUsers(response.data.items)
                 })
